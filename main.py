c693ee188c01a9e317a4bf6dae25045b918b831c
dilshodev-py <absaitovdev@gmail.com>
2025-07-22 10:31:19 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+
+# Pycharm + GitHub
+#
+
+"""
+git config --global user.name "dilshodev-py"
+git config --global user.email "absaitovdev@gmail.com"
+"""
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/dilshodev-py/GitHubProject.git
+git push -u origin main
+"""
