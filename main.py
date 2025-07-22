a01f7fe4e18facac0c949e48bb7c59bbbb3d5b4e
Jahongir Samatov <jahongirsamatov676@gmail.com>
2025-07-22 11:15:02 +0500

    add print

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print("Sulton Sadullayevv")
 print("Sitora Kodirkulova")
 print("Haminjonov Abdurashid")
 print("Sulton Sadullayevv")
-print("Samatov Jahongir")
+print("Samatov Jahongirr")
 
 # Pycharm + GitHub
 # Abdumalik Yul...
